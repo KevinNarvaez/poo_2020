91f257fbe5774687539676d7a64168a57e485f51
KevinNarvaez <56696280+KevinNarvaez@users.noreply.github.com>
2020-05-19 10:42:09 -0500

    Add files via upload

---
+++ clase6/src/EjecutaEmpleado.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USUARIO
+ */
+public class EjecutaEmpleado {
+   public static void main(String[] args) {
+        String nombre;
+        int horas;
+        double cuota;
+        char desea = 's';
+        do {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Ingrese el nombre del empleado");
+            nombre = sc.nextLine();
+            System.out.println("Ingrese el nº de horas trabajadas: ");
+            horas = sc.nextInt();
+            System.out.println("Ingrese la cuota por hora ");
+            cuota = sc.nextDouble();
+            Empleado empleado = new Empleado(nombre, horas, cuota);
+            empleado.calcularSueldo();
+            System.out.println("Nombre del empleado: " + empleado.getNombre());
+            System.out.println("El sueldo es: " + empleado.getSueldo());
+
+            System.out.println("¿Desea ingresar otro empleado s/n?");
+            desea = sc.next().toLowerCase().charAt(0);
+        } while (desea == 's');
+
+
+      
+    }
+
+}
